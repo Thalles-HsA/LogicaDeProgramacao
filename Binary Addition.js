8c24db913626b960569ab47f152739585defc4e1
Thalles-HsA <thsa.henrique@gmail.com>
2022-12-21 00:00:53 -0300

    Adicionando estudo

---
+++ Binary Addition.js
@@ -0,0 +1,27 @@
+function addBinary(a,b) {
+
+    let soma = a + b
+    let acc = []
+    
+    while (soma >= 2 ) { 
+            acc.push(Math.trunc(soma) % 2)
+            soma /=  2
+    }
+
+    acc.push(Math.trunc(soma))
+    let binario = acc.reverse().join('')
+  
+    return binario
+
+}
+
+
+// metodo ultra mega fácil.
+
+function addBinary2(a,b){
+    return (a+b).toString(2)
+}
+
+console.log(addBinary(5, 9))
+console.log(addBinary2(5, 9))
+
