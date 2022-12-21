76a91a190604862ed7bb81bb8e65aef5182beeef
Thalles-HsA <thsa.henrique@gmail.com>
2022-12-21 00:01:12 -0300

    Adicionando estudo

---
+++ Localize The Barycenter of a Triangle.js
@@ -0,0 +1,12 @@
+function barTriang(p1, p2, p3){
+    
+   let cx = (p1[0] + p2 [0] + p3 [0]) / 3
+   let cy = (p1[1] + p2 [1] + p3 [1]) / 3
+
+   let arr = [parseFloat(cx.toFixed(4)), parseFloat(cy.toFixed(4))]
+
+   return arr
+  
+  }
+
+console.log(barTriang([4, 6], [12, 4], [10, 10]))
