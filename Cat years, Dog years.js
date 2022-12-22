98838f8770a805749cb895207dad1e5bb2e11568
Thalles-HsA <thsa.henrique@gmail.com>
2022-12-22 11:46:16 -0300

    Novos exercicios

---
+++ Cat years, Dog years.js
@@ -0,0 +1,25 @@
+var humanYearsCatYearsDogYears = function(humanYears) {
+  
+    let dogYears = 0
+    let catYears = 0
+
+    if(humanYears == 1) {
+        dogYears += 15
+        catYears += 15
+
+        return [humanYears, catYears, dogYears]
+    } else if ( humanYears == 2 ) {
+        dogYears += 15 + 9
+        catYears += 15 + 9
+
+        return [humanYears, catYears, dogYears]
+    } else if ( humanYears >= 3) {
+        dogYears += 15 + 9 + (5 *(humanYears-2))
+        catYears += 15 + 9 + (4 *(humanYears-2))
+
+        return [humanYears, catYears, dogYears]
+    }
+
+  }
+
+  console.log(humanYearsCatYearsDogYears(4))
